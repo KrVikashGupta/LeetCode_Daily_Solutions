ee4f65e76ac9da08d13b2c75ffc97a0fe3cd9940
Vikash Kumar Gupta <120501092+KrVikashGupta@users.noreply.github.com>
2025-08-06 19:36:14 +0530

    Update . NextPermutation.java

--- . NextPermutation.java
+++ . NextPermutation.java
@@ -11,7 +11,6 @@ Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
 While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
 Given an array of integers nums, find the next permutation of nums.
 
-The replacement must be in place and use only constant extra memory.
 
 
  
@@ -88,4 +87,5 @@ class Solution {
 Time complexity: |O(n)|
 Space complexity:|O(1)|
 */ 
+
 
